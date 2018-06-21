c644b7e6671d9b5b8fd0ef0e0ee2c611aabd67b9
Nora Maguire <eva@rigel.moe>
2018-06-21 07:17:01 -0500

    Working on chatbot side now slightly (sorry Marz!)
    Got the Database loading to work (demoing it with that showing all twitch users)

--- chatbot/core.rb
+++ chatbot/core.rb
@@ -1,11 +1,15 @@
-Bundler.require(:default)
-APPDIR=File.dirname(__FILE__)
-KEYS = JSON.parse(File.open("#{APPDIR}/keys.json").read)
+require "rubygems"
+require "bundler/setup"
+require "active_record"
+require 'erb'
 CMDRSHEPARD=true;
-begin
-    con=PG.connect :dbname =>'CmdrShepard_development', :user=>'postgres'
-    puts con.server_version
-rescue => exception
-    
-end
+#Getting Active REcord set up (Taken from https://github.com/jwo/ActiveRecord-Without-Rails/blob/master/ar-no-rails.rb)
+project_root = File.dirname(File.absolute_path(__FILE__))
+Dir.glob(project_root + "/app/models/*.rb").each{|f| require f}
 
+connection_details = YAML::load(ERB.new(File.read('config/database.yml')).result)
+ActiveRecord::Base.establish_connection(connection_details)
+#Just getting the list of all twitch users to demo the fact that active record is loading them
+TwitchUser.all.each{|user|
+    puts user.name
+}
